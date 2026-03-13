8646fe7175900f95684e603c4bb8f70b78654810
TeeraphatP2 <teerapat200001@gmail.com>
2026-03-14 01:31:30 +0700

    add navbar.php for navbar on frontend to git

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <ul>
+        <li><a href="./">หน้าแรก</a></li>
+        <li><a href="./?page=about">เกี่ยวกับ</a></li>
+    </ul>
+    <div class="button-login">
+        <a href="./?page=login">ล็อคอิน</a>
+    </div>
+    <div class="button-register">
+        <a href="./?page=register">สมัครสมาชิก</a>
+    </div>
+</nav>
