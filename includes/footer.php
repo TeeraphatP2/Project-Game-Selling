cd6df4c38397e43b3b0c63f7bb62d54383f82984
TeeraphatP2 <teerapat200001@gmail.com>
2026-03-14 01:29:03 +0700

    add footer.php for make footer on frontend to git

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer-conteiner">
+    <div class="footer">
+        <div class="footer-left">
+            <h2>2025 Company,Inc</h2>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <li><a href="./">หน้าแรก</a></li>
+                <li><a href="#">นโยบาย</a></li>
+                <li><a href="./?page=about">เกี่ยวกับ</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
