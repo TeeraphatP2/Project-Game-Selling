0854beba41ac536148c7e39ad0b4b8e8126e7731
TeeraphatP2 <teerapat200001@gmail.com>
2026-03-14 01:30:32 +0700

    add head.php for header on frontend to git

---
+++ includes/head.php
@@ -0,0 +1,35 @@
+<head>
+    <?php 
+        session_start();
+        
+        include './config/conn.php';
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameSellig</title>
+    <link rel="stylesheet" href="./assets/css/frontend/index.css">
+    <link rel="stylesheet" href="./assets/css/frontend/navbar/navbar.css">
+    <link rel="stylesheet" href="./assets/css/frontend/main.css">
+    <link rel="stylesheet" href="./assets/css/frontend/footer/footer.css">
+    <link rel="stylesheet" href="./assets/css/frontend/login.css">
+    <link rel="stylesheet" href="./assets/css/frontend/register.css">
+    <link rel="stylesheet" href="./assets/css/frontend/homepage.css">
+    <link rel="stylesheet" href="./assets/css/frontend/aboutUs.css">
+    <?php 
+        $bgPage = $_GET['page'] ?? ""; 
+        switch($bgPage) {
+            case '':
+                echo '<link rel="stylesheet" href="./assets/css/frontend/backgroundBody/homepage.css">';
+                break;
+            case 'about':
+                echo '<link rel="stylesheet" href="./assets/css/frontend/backgroundBody/homepage.css">';
+                break;
+            case 'login':
+                echo '<link rel="stylesheet" href="./assets/css/frontend/backgroundBody/auth.css">';
+                break;
+            case 'register':
+                echo '<link rel="stylesheet" href="./assets/css/frontend/backgroundBody/auth.css">';
+                break;
+        }
+    ?>
+</head>
