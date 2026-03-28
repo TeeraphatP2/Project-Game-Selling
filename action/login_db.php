e063e6bcceefbfec82426c538d40feee9f6219a7
TeeraphatP2 <teerapat200001@gmail.com>
2026-03-29 01:44:45 +0700

    update login_db.php connect database and report to json

--- action/login_db.php
+++ action/login_db.php
@@ -3,12 +3,12 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 session_start();
-$conn = require dirname(__dir__) . '/config/conn.php';
+$conn     = require dirname(__dir__) . '/config/conn.php';
 
-$input = file_get_contents('php://input');
-$data = json_decode($input, true);
+$input    = file_get_contents('php://input');
+$data     = json_decode($input, true);
 
-$email = $data['email'];
+$email    = $data['email'];
 $password = $data['password'];
 
 if(empty($email) && empty($password)){
@@ -21,26 +21,26 @@ if(empty($email) && empty($password)){
     echo json_encode(["massage" => "กรุณากรอกรหัสผ่าน"]);
     die();
 }
-
+//echo json_encode(["massage" => $email, "password" => $password]);
 try{
-$query_check_email = "SELECT firstname, email, `password` FROM users WHERE email = :email";
+$query_check_email      = "SELECT firstname, email, `password` FROM users WHERE email = :email";
 $stmt_query_check_email = $conn->prepare($query_check_email);
 $stmt_query_check_email->bindParam(':email', $email, PDO::PARAM_STR);
 $stmt_query_check_email->execute();
-$call_back_check_email = $stmt_query_check_email->fetch(PDO::FETCH_ASSOC);
+$call_back_check_email  = $stmt_query_check_email->fetch(PDO::FETCH_ASSOC);
+
 if($call_back_check_email['email'] == $email){
     $hash = $call_back_check_email['password'];
 
     if(password_verify($password, $hash)){
-        $_SESSION['success'] = $call_back_check_email['firstname'];
-        header("Location: ../?page=login");
-        die();
+        echo json_encode(["status" => "success", "firstname" => $call_back_check_email['firstname']]);
+        exit();
     }else{
         echo json_encode(["massage" => "รหัสผ่านไม่ถูกต้อง"]);
         exit();
     }
 }
 }catch(PDOException $e){
-    echo json_encode(["Query Error" => $e]);
+    echo json_encode(["Query Error" => "dsdsd"]);
     die();
 }
