21b0b8963936b342a0b658aac6af5d5d64949909
TeeraphatP2 <teerapat200001@gmail.com>
2026-05-30 09:52:01 +0700

    refactor and fix: use throwing exception and fix logic

--- repositories/UserRepository.php
+++ repositories/UserRepository.php
@@ -2,7 +2,8 @@
 
 namespace App\Repositories;
 
-
+use PDOException;
+//  query สำหรับ users
 class UserRepository
 {
     private \PDO $pdo;
@@ -12,58 +13,65 @@ class UserRepository
         $this->pdo = $pdo;
         
     }
-
-    public function findByEmail(string $email)
+    // หาข้อมูล user ผ่าน email
+    public function findByEmail(string $email): ?array
     {
+        try {
+            $statement = $this->pdo->prepare("SELECT userId, firstname, email, `password` FROM users WHERE email = :email");
+            $statement->bindParam(':email', $email, \PDO::PARAM_STR);
+            $statement->execute();
+            $userData = $statement->fetch(\PDO::FETCH_ASSOC);
 
+            if($userData === false){
+                return null;
+            }
+
+            return $userData;
+        }catch (PDOException $e) {
+    // ส่ง error code พร้อมข้อความ error เมื่อ query ผิดผลาด
+            throw new PDOException('PDO_EXCEPTION_ERROR', previous: $e);
+            // throw new \RuntimeException('PDO_EXCEPTION_ERROR', 0, $e);
+        }
+    }
+
+    // ระบบล็อคอิน
+    public function userLogin(string $email): ?array
+    {
         try {
             $statement = $this->pdo->prepare("SELECT firstname, email, `password` FROM users WHERE email = :email");
             $statement->bindParam(':email', $email, \PDO::PARAM_STR);
             $statement->execute();
             return $statement->fetch(\PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-
-            return ['message' => 'PDO_EXCEPTION_ERROR','data' => $e->getMessage()];
+        }catch (PDOException $e) {
+            throw new PDOException('PDO_EXCEPTION_ERROR', previous: $e);
         }
     }
 
-    public function userLogin(string $email)
+    // ระบบสมัครสมาชิก
+    public function userRegister(string $firstname, string $lastname, string $email, string $passwordHash): bool
     {
-        try {
-            $statement = $this->pdo->prepare("SELECT firstname, email, `password` FROM users WHERE email = :email");
-            $statement->bindParam(':email', $email, \PDO::PARAM_STR);
-            $statement->execute();
-            return $statement->fetch(\PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-
-            return ['message' => 'PDO_EXCEPTION_ERROR','data' => $e->getMessage()];
-        }
-    }
-    public function userRegister(string $firstname, string $lastname, string $email, string $passwordHash){
 
         try{
 
             if($this->findByEmail($email)){
                 return false;
-
             }else{
-                
                 $statement = $this->pdo->prepare
                 (
                 "INSERT INTO users (firstname, lastname, email, `password`, `role`)
-                VALUES (':firstname', ':lastname', ':email', ':password', ':role')"
+                VALUES (:firstname, :lastname, :email, :password, :role)"
                 );
                 $statement->bindParam(':firstname', $firstname, \PDO::PARAM_STR);
                 $statement->bindParam(':lastname', $lastname, \PDO::PARAM_STR);
                 $statement->bindParam(':email', $email, \PDO::PARAM_STR);
                 $statement->bindParam(':password', $passwordHash, \PDO::PARAM_STR);
-                $statement->bindParam(':role', 'user', \PDO::PARAM_STR);
+                $statement->bindValue(':role', 'user', \PDO::PARAM_STR);
                 return $statement->execute();
-                
             }
-        }catch(\PDOException $e){
-            
-            return ['message' => 'PDO_EXCEPTION_ERROR','data' => $e->getMessage()];
+
+        }catch(PDOException $e){
+    // ส่ง error code พร้อมข้อความ error เมื่อ query ผิดผลาด
+            throw new PDOException('PDO_EXCEPTION_ERROR', previous: $e);
         }
     }
 }
