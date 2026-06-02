82e269a6a41396a7ecf02e6a99e8beb6fd5d1e7c
TeeraphatP2 <teerapat200001@gmail.com>
2026-06-02 14:19:41 +0700

    refactor: fix file path

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar">
     <ul>
         <li><a href="./">หน้าแรก</a></li>
-        <li><a href="./?page=about">เกี่ยวกับ</a></li>
+        <li><a href="./?views=about">เกี่ยวกับ</a></li>
     </ul>
     <div class="button-login">
-        <a href="./?page=login">ล็อคอิน</a>
+        <a href="./?views=login">ล็อคอิน</a>
     </div>
     <div class="button-register">
-        <a href="./?page=register">สมัครสมาชิก</a>
+        <a href="./?views=register">สมัครสมาชิก</a>
     </div>
 </nav>
