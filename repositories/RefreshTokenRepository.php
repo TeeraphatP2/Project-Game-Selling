08bad880b8589f5a4092c06b8922e6ea89a0cfc7
TeeraphatP2 <teerapat200001@gmail.com>
2026-09-20 00:11:09 +0700

    feat: add new insertToDatabase fucntion

--- repositories/RefreshTokenRepository.php
+++ repositories/RefreshTokenRepository.php
@@ -1,8 +1,19 @@
 <?php
 
 namespace App\Repositories;
-
+use PDO;
+use PDOException;
 
 Class RefreshTokenRepository {
+    private PDO $pdo;
     
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    public function insertToDatabase(string $refreshTokenHash, int $expireAt)
+    {
+        
+    }
 }
